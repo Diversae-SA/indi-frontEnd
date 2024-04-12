75ef8657a8033514222d758a27ba35c63618c497
tic-pima <alverromero11@gmail.com>
2024-04-11 20:03:32 -0400

    actualizando el headers

--- src/composable/useFetch.ts
+++ src/composable/useFetch.ts
@@ -2,11 +2,15 @@ import { ofetch } from 'ofetch'
 import { useUserSession } from '/@src/stores/userSession'
 
 export function useFetch() {
+  const userSession = useUserSession()
   return ofetch.create({
     baseURL: import.meta.env.VITE_API_BASE_URL + '/api/v1/' ?? 'http://localhost:8080',
+    credentials: 'include',
+    headers: {
+      'Content-Type': 'application/json',
+      'X-Requested-With': 'XMLHttpRequest',
+    },
     onRequest: ({ options }) => {
-      const userSession = useUserSession()
-
       if (userSession.isLoggedIn) {
         options.headers = {
           ...options.headers,
