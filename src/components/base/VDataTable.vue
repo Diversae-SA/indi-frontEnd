297e31a55ee86930be2d32739f5d1d1e9d26c1fe
tic-pima <alverromero11@gmail.com>
2024-05-24 07:49:12 -0400

    ajustes para la tabla

--- src/components/base/VDataTable.vue
+++ src/components/base/VDataTable.vue
@@ -82,8 +82,8 @@ const options = ref({
   ],
   processing: true,
   language: {
-    // url: '//cdn.datatables.net/plug-ins/1.13.7/i18n/es-ES.json',
-    url: '/api/plug-ins/1.13.7/i18n/es-ES.json',
+    url: 'https://cdn.datatables.net/plug-ins/1.13.7/i18n/es-ES.json',
+    // url: '/api/plug-ins/1.13.7/i18n/es-ES.json',
     lengthMenu: '_MENU_',
     search: ' ',
     searchPlaceholder: 'Buscar...',
